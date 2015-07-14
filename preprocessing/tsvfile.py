5be3b07da25169456e14ce888dac8516df342297
gwg <garth.w.griffin@gmail.com>
2015-07-14 11:31:16 -0400

    Small formatting adjustment in file headers.

--- preprocessing/tsvfile.py
+++ preprocessing/tsvfile.py
@@ -16,8 +16,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
 Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
-Antislavery Petitions Visualization.  If not, see
-<http://www.gnu.org/licenses/>.
+Antislavery Petitions Visualization. If not, see <http://www.gnu.org/licenses/>.
 ________________________________________________________________________________
 
 Author: Garth Griffin (http://garthgriffin.com)
